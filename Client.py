fc2b52db34ea9856f91be0593f10643e2decdd20
prabashwara65 <prabashwara65@gmail.com>
2025-03-13 16:07:31 +0530

    commit

--- Client.py
+++ Client.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 
+HEADER = 64
 PORT= 5000
 
 SERVER = socket.gethostbyname(socket.gethostname())
@@ -11,13 +12,21 @@ ADDR = (SERVER , PORT)
 server = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
 server.bind(ADDR )
 
-def handle_client(con , addr):
-    pass
+def handle_client(conn , addr):
+    print(f"[NEW CONNECTION] {addr} connected!")
+
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER)
+
 
 def start():
     server.listen()
     while True:
         conn , addr = server.accept()
+        thread = threading.Thread(target=handle_client , argus=(conn , addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTION] {threading.active_count() - 1}")
 
 
 print("[STARTING] Server is startting...")  
