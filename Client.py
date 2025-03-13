a37a791c96d8004f5a8acdd1af9cea971dd088b2
prabashwara65 <prabashwara65@gmail.com>
2025-03-13 16:42:58 +0530

    done

--- Client.py
+++ Client.py
@@ -1,44 +1,29 @@
 import socket
-import threading
 
 HEADER = 64
 PORT= 5000
-
-SERVER = socket.gethostbyname(socket.gethostname())
-print(SERVER)
-
 FORMAT = 'utf- 8'
 DISSCONNECT_MESSAGE = "!!DISSCONNECT"
-
+SERVER = '127.0.0.1'
 ADDR = (SERVER , PORT)
 
-server = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
-server.bind(ADDR )
-
-def handle_client(conn , addr):
-    print(f"[NEW CONNECTION] {addr} connected!")
-
-    connected = True
-    while connected:
-        msg_length = conn.recv(HEADER).decode(FORMAT)
-        msg_length = int(msg_length)
-        msg = conn.recv(msg_length).decode(FORMAT)
-        if msg == DISSCONNECT_MESSAGE:
-            connected = False
-
-        print(f"[{addr}] {msg}")
-
-    conn.close()    
-
-def start():
-    server.listen()
-    while True:
-        conn , addr = server.accept()
-        thread = threading.Thread(target=handle_client , argus=(conn , addr))
-        thread.start()
-        print(f"[ACTIVE CONNECTION] {threading.active_count() - 1}")
+client = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
+client.connect(ADDR)
 
 
-print("[STARTING] Server is startting...")  
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
 
-start()
+# Send messages
+send("Hello from Client!")
+send("How are you, Server?")
+
+# Disconnect the client
+send(DISCONNECT_MESSAGE)
+
+
